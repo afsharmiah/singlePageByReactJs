52f07a487fd4ecf4e33e51c68d789660aaf6fcb1
afsharmiah <afsharmiah15@gmail.com>
2019-06-11 10:46:07 -0400

    Single page react app

---
+++ myapp2/src/Contact.js
@@ -0,0 +1,17 @@
+import React,{Component} from "react";
+import {Link} from "react-router";
+
+class Contact extends Component{
+	render(){
+		return(
+			<div>
+        <h2>GOT QUESTIONS?</h2>
+        <p>The easiest thing to do is post on
+        our <a href="http://forum.kirupa.com">forums</a>.
+        </p>
+      </div>
+    );
+  }
+}
+ 
+export default Contact;
